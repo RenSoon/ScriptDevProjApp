94ea1229accd851f5a424d0f07ad4965227dcad6
RenSoon <sbrendonsamson@gmail.com>
2022-10-14 12:06:51 +0800

    added companents and pages

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -2,7 +2,7 @@
     <div style="height:100vh;font-family: Ringbearer;background: url(/Rivendell.png)  center / cover no-repeat">
         <div class="d-flex justify-content-center align-items-center justify-content-xxl-center align-items-xxl-center" style="height: inherit;width: inherit;">
             <div class="d-flex flex-column">
-                <div class="container-fluid myClass">
+                <div class="container-fluid myClass" style=": #d9db86 !important;font-size: 10vh;font-family: Ringbearer;">
                     This is the Dashboard Page
                 </div>
             </div>
@@ -11,12 +11,7 @@
 </template>
 
 <script>
-</script>
-
-<style scoped>
-.myClass{
-    color: #d9db86 !important;
-    font-size: 10vh;
-    font-family: Ringbearer;
+export default{
+    name: 'Dashboard'
 }
-</style>
+</script>
